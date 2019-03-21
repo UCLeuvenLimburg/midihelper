f6c408dbd929323d82bb321f440d9c9c615ca787
Frederic Vogels <frederic.vogels@ucll.be>
2019-03-21 16:45:39 +0100

    notes command

--- midihelper/command_line.py
+++ midihelper/command_line.py
@@ -49,6 +49,80 @@ def _dump(args):
                 print(result)
 
 
+
+def _notes(args):
+    class Note:
+        def __init__(self, start, duration, channel, note_index, instrument):
+            self.start = start
+            self.duration = duration
+            self.channel = channel
+            self.note_index = note_index
+            self.instrument = instrument
+
+        def format(self, format):
+            format = format.replace('%c', str(self.channel))
+            format = format.replace('%n', str(self.note_index))
+            format = format.replace('%s', str(self.start))
+            format = format.replace('%e', str(self.start + self.duration))
+            format = format.replace('%d', str(self.duration))
+            format = format.replace('%i', str(self.instrument))
+
+            return format
+
+    filename = args.filename
+    format = args.format
+    midi = mido.MidiFile(filename)
+    notes = []
+
+    for track in midi.tracks:
+        time = 0
+        note_starts = [ -1 ] * 128
+        channel_instruments = [ 0 ] * 16
+
+        def process_note_on(channel, dt, note_index, velocity):
+            nonlocal time, note_starts
+            if velocity == 0:
+                process_note_off(channel, dt, note_index, velocity)
+            elif note_starts[note_index] != -1:
+                process_note_off(channel, dt, note_index, 0)
+                process_note_on(channel, 0, note_index, velocity)
+            else:
+                time += dt
+                note_starts[note_index] = time
+
+        def process_note_off(channel, dt, note_index, velocity):
+            nonlocal time, channel_instruments, note_starts
+            time += dt
+            start = note_starts[note_index]
+            duration = time-start
+            instrument = channel_instruments[channel]
+            note = Note(start=start, duration=duration, note_index=note_index, instrument=instrument, channel=channel)
+            notes.append(note)
+            note_starts[note_index] = -1
+
+
+        def process_program_change(channel, dt, instrument):
+            nonlocal time, channel_instruments
+            channel_instruments[channel] = instrument
+            time += dt
+
+        for message in track:
+            if message.type == 'note_on':
+                process_note_on(message.channel, message.time, message.note, message.velocity)
+            elif message.type == 'note_off':
+                process_note_off(message.channel, message.time, message.note, message.velocity)
+            elif message.type == 'program_change':
+                process_program_change(message.channel, message.time, message.program)
+            else:
+                time += message.time
+
+    notes.sort(key=lambda n: n.start)
+
+    for note in notes:
+        print(note.format(format))
+
+
+
 def _create_command_line_arguments_parser():
     '''
     Creates parsers and subparsers
@@ -63,6 +137,11 @@ def _create_command_line_arguments_parser():
     subparser.add_argument('--filter', help='Filter', default='*')
     subparser.add_argument('--no-events', help="Don't print events", action='store_const', const='', dest='filter')
     subparser.set_defaults(func=_dump)
+
+    subparser = subparsers.add_parser('notes', help='prints notes')
+    subparser.add_argument('filename', help='MIDI file name')
+    subparser.add_argument('--format', help='Format', default='channel=%c note=%n start=%s duration=%d instrument=%i')
+    subparser.set_defaults(func=_notes)
 
     subparser = subparsers.add_parser('play', help='plays MIDI file')
     subparser.add_argument('filename', help='MIDI file name')
